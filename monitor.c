80ce2e838d08683c4c80e43f43228eb755541c63
RaduTek <me@radutek.uk>
2025-05-07 23:42:25 +0300

    monitor and treasure hub for phase 2

---
+++ monitor.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <time.h>
+
+#define MAX_PATH 256
+#define CMD_FILE "/tmp/treasure_hunt_monitor_cmd"
+
+volatile sig_atomic_t monitor_got_signal = 0;
+
+void signal_handler(int signum) {
+    monitor_got_signal = signum;
+}
+
+void got_signal(int signum) {
+    switch (signum) {
+        case SIGUSR1:
+            // read command from file
+            int fd = open(CMD_FILE, O_RDONLY);
+            if (fd != -1) {
+                char command[MAX_PATH];
+                ssize_t bytes_read = read(fd, command, sizeof(command) - 1);
+                if (bytes_read > 0) {
+                    command[bytes_read] = '\0';
+                    command[strcspn(command, "\n")] = 0;
+
+                    int ret = system(command);
+                    if (ret != 0) {
+                        printf("monitor: command returned code %d", ret);
+                    }
+                } else {
+                    printf("monitor: error reading command");
+                }
+                close(fd);
+            } else {
+                printf("monitor: error opening command file");
+            }
+            break;
+        case SIGTERM:
+            // exit gracefully
+            printf("monitor: received SIGTERM, stopping...\n");
+            unlink(CMD_FILE);
+            exit(0);
+        default:
+            break;
+    }
+}
+
+int main() {
+    // Set up the signal handler
+    struct sigaction sa;
+    sa.sa_flags = 0;
+    sa.sa_handler = signal_handler;
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGUSR1, &sa, NULL);
+
+    while(1) {
+        // Wait for signals
+        pause();
+
+        // Check if we got command
+        if (monitor_got_signal) {
+            got_signal(monitor_got_signal);
+        }
+    }
+}
--- treasure_hub.c
+++ treasure_hub.c
@@ -1,19 +1,73 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 
 #define MAX_PATH 256
 #define MAX_ARGC 32
+#define CMD_FILE "/tmp/treasure_hunt_monitor_cmd"
 
 
 bool monitor_started = false;
+bool monitor_stopping = false;
+pid_t monitor_pid = 0;
+
+
+void handle_sigchld(int signum) {
+    wait(NULL);
+    monitor_started = false;
+    monitor_stopping = false;
+    printf("Monitor has exited\n");
+}
+
+void setup_signal() {
+    struct sigaction sa;
+    sa.sa_handler = handle_sigchld;
+    sa.sa_flags = 0;
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGCHLD, &sa, NULL);
+}
+
+void send_command(char *command) {
+    if (strlen(command) >= MAX_PATH) {
+        printf("error: command exceeds maximum length of %d\n", MAX_PATH - 1);
+        return;
+    }
+
+    int fd = open(CMD_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd < 0) {
+        perror("error: open");
+        return;
+    }
+
+    if (write(fd, command, strlen(command)) < 0) {
+        perror("error: write");
+        close(fd);
+        return;
+    }
+
+    close(fd);
+
+    if (kill(monitor_pid, SIGUSR1) < 0) {
+        perror("error: kill");
+    }
+}
 
 
 int cmd_start_monitor(int argc, char *argv[]) {
     if (!monitor_started) {
-        monitor_started = true;
         // start the monitor
-        printf("NOT IMPLEMENTED!\n");
+        monitor_pid = fork();
+        if (monitor_pid == 0) {
+            execl("./monitor", "monitor", NULL);
+            exit(1);
+        }
+
+        monitor_started = true;
+        monitor_stopping = false;
         printf("Monitor has been started!\n");
     } else {
         printf("Monitor already started!\n");
@@ -23,34 +77,65 @@ int cmd_start_monitor(int argc, char *argv[]) {
 }
 
 int cmd_list_hunts(int argc, char *argv[]) {
-    printf("NOT IMPLEMENTED!\n");
+    if (!monitor_started) {
+        printf("Monitor is not started!\n");
+        return 1;
+    }
+
+    char command[MAX_PATH];
+    snprintf(command, sizeof(command), "ls ./hunts/");
+    send_command(command);
 
     return 0;
 }
 
 int cmd_list_treasures(int argc, char *argv[]) {
-    printf("NOT IMPLEMENTED!\n");
+    if (!monitor_started) {
+        printf("Monitor is not started!\n");
+        return 1;
+    }
+
+    if (argc < 2) {
+        printf("Bad arguments!\n");
+        return 1;
+    }
+
+    char command[MAX_PATH];
+    snprintf(command, sizeof(command), "./treasure_manager --list %s", argv[1]);
+    send_command(command);
 
     return 0;
 }
 
 int cmd_view_treasure(int argc, char *argv[]) {
-    printf("NOT IMPLEMENTED!\n");
-
-    printf("I got %d arguments:\n", argc);
-    for (int i=0; i < argc; i++) {
-        printf("argv[%d]: %s\n", i, argv[i]);
+    if (!monitor_started) {
+        printf("Monitor is not started!\n");
+        return 1;
     }
+
+    // printf("I got %d arguments:\n", argc);
+    // for (int i=0; i < argc; i++) {
+    //     printf("argv[%d]: %s\n", i, argv[i]);
+    // }
+
+    if (argc < 3) {
+        printf("Bad arguments!\n");
+        return 1;
+    }
+
+    char command[MAX_PATH];
+    snprintf(command, sizeof(command), "./treasure_manager --view %s %s", argv[1], argv[2]);
+    send_command(command);
 
     return 0;
 }
 
 int cmd_stop_monitor(int argc, char *argv[]) {
     if (monitor_started) {
-        monitor_started = false;
         // stop the monitor
-        printf("NOT IMPLEMENTED!\n");
-        printf("Monitor stopped!\n");
+        kill(monitor_pid, SIGTERM);
+        monitor_started = false;
+        monitor_stopping = true;
     } else {
         printf("Monitor is not started!\n");
     }
@@ -105,6 +190,10 @@ int parse_interactive_command() {
 
         if (argc == 0) continue;
 
+        if (monitor_stopping) {
+            printf("error: monitor is stopping\n");
+        }
+
         if (strcmp(argv[0], "start_monitor") == 0) {
             cmd_start_monitor(argc, argv);
         } else if (strcmp(argv[0], "list_hunts") == 0) {
@@ -130,5 +219,7 @@ int parse_interactive_command() {
 }
 
 int main() {
+    setup_signal();
+
     return parse_interactive_command();
 }
